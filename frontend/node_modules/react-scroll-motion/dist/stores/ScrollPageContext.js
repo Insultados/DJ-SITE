20fa5bec989012932e55dbf3d28bb0fd6285444f
insultados <qskov200155@gmail.com>
2023-08-05 12:33:23 +0000

    frontend commit

---
+++ frontend/node_modules/react-scroll-motion/dist/stores/ScrollPageContext.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ScrollPageContext = void 0;
+var react_1 = require("react");
+var constants_1 = require("../constants");
+exports.ScrollPageContext = (0, react_1.createContext)(constants_1.initialScrollPage);
---
+++ frontend/node_modules/react-scroll-motion/dist/stores/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ScrollPageContext = exports.ScrollDataContext = void 0;
+var ScrollDataContext_1 = require("./ScrollDataContext");
+Object.defineProperty(exports, "ScrollDataContext", { enumerable: true, get: function () { return ScrollDataContext_1.ScrollDataContext; } });
+var ScrollPageContext_1 = require("./ScrollPageContext");
+Object.defineProperty(exports, "ScrollPageContext", { enumerable: true, get: function () { return ScrollPageContext_1.ScrollPageContext; } });
