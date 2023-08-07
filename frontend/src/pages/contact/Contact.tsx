655600f2a6931d872a96ef9e187c72f7e65add24
Insultados <qskov200155@gmail.com>
2023-08-07 22:16:11 +0300

    frontend commit

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import classes from './Contact.module.css'
 import { Fade, Slide } from "react-awesome-reveal";
+import { useState } from 'react';
+
 
 
 const Contact = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        product_name: ''
+      })
+    
+      const changeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+        setForm({ ...form, [event.currentTarget.name]: event.currentTarget.value })
+      }
+
+      const showState = () => {
+        console.log(form)
+      }
+    
+
+
     return (
         <div className={classes.page_container}>
             <div className={classes.page_head}>
@@ -14,25 +34,26 @@ const Contact = () => {
                     <Fade triggerOnce cascade damping={0.25}>
                         <div className={classes.contact_form}>
                             <label >ФИО </label>
-                            <input type="text" name="name" id="name" required />
+                            <input onChange={changeHandler} type="name" name="name" id="name" required />
                         </div>
                         <div className={classes.contact_form}>
                             <label>Номер телефона </label>
-                            <input type="phone" name="phone" id="phone" required />
+                            <input onChange={changeHandler} type="phone" name="phone" id="phone" required />
                         </div>
                         <div className={classes.contact_form}>
                             <label>E-mail: </label>
-                            <input type="email" name="email" id="email" required />
+                            <input onChange={changeHandler} type="email" name="email" id="email" required />
                         </div>
                         <div className={classes.contact_form}>
                             <label>Название товара: </label>
-                            <input type="name" name="name" id="name" required />
+                            <input onChange={changeHandler} type="product_name" name="product_name" id="product_name" required />
                         </div>
                     </Fade>
                     <Slide cascade triggerOnce damping={0.25}>
-                    <div className={classes.contact_button}>
-                        <input type="submit" value="Отправить" />
-                    </div>
+                        <div className={classes.contact_button}>
+                            <input type="button" value="Отправить" onClick={showState} />
+
+                        </div>
                     </Slide>
 
                 </form>
