6c9e90a3e80438fa239766e8bcdb6c35065ebf3b
Insultados <qskov200155@gmail.com>
2023-08-07 22:32:15 +0300

    add reading input data commit

--- frontend/src/pages/feedback/Feedback.tsx
+++ frontend/src/pages/feedback/Feedback.tsx
@@ -2,8 +2,26 @@ import React from "react";
 import classes from './Feedback.module.css'
 import FeedbackCard from "../../components/feedback_card/FeedbackCard";
 import { Fade, Slide } from "react-awesome-reveal";
+import { useState } from "react";
 
 const Feedback = () => {
+
+
+    const [form, setForm] = useState({
+        name: '',
+        product_name: '',
+        review: ''
+      })
+    
+      const changeHandler = (event: React.FormEvent<HTMLInputElement>|React.ChangeEvent<HTMLTextAreaElement>) => {
+        setForm({ ...form, [event.currentTarget.name]: event.currentTarget.value })
+      }
+
+      const showState = () => {
+        console.log(form)
+      }
+    
+
 
     return (
         <div className={classes.page_container}>
@@ -16,20 +34,20 @@ const Feedback = () => {
                     <Fade damping={0.25} triggerOnce cascade>
                         <div className={classes.feedback_form}>
                             <label >Ваше имя </label>
-                            <input type="text" name="name" id="name" required />
+                            <input onChange={changeHandler} type="text" name="name" id="name" required />
                         </div>
                         <div className={classes.feedback_form}>
                             <label>Название товара </label>
-                            <input type="text" name="text" id="text" required />
+                            <input onChange={changeHandler} type="text" name="product_name" id="product_name" required />
                         </div>
                         <div className={classes.feedback_form}>
                             <label className={classes.textarea_label}>Ваш отзыв о товаре</label>
-                            <textarea name="text" id="text" required />
+                            <textarea onChange={changeHandler} name="review" id="review" required />
                         </div>
                     </Fade>
                     <Slide damping={0.25} triggerOnce cascade direction={"up"} >
                         <div className={classes.feedback_button}>
-                            <input type="submit" value="Отправить" />
+                            <input type="button" value="Отправить" onClick={showState} />
                         </div>
                     </Slide>
                 </form>
@@ -41,7 +59,7 @@ const Feedback = () => {
                 <Fade triggerOnce cascade damping={0.25}>
                     <FeedbackCard name={'Мария'} product={'Фассад Бифлекс'} feedback={'Отличный товар! Хорошее качество и своевременная доставка!'} />
                     <FeedbackCard name={'Елена'} product={'Фассад Бифлекс'} feedback={'Отличный товар! Хорошее качество и своевременная доставка!'} />
-                </Fade>
+                </Fade> 
             </div>
         </div>
     );
